7b3502cb400cf5226836ff82bd2fcc6b08a7f2b7
fast1sh <dlemeshko04@gmail.com>
2019-05-02 17:47:28 +0300

    Add layout and script

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log('hi');
+//UI
+const sumButton = document.getElementById('sum-button');
+const firstNumberInput = document.getElementById('first-number');
+const secondNumberInput = document.getElementById('second-number');
+
+const sum = () => {
+  const firstNumber = Number(firstNumberInput.value);
+  const secondNumber = Number(secondNumberInput.value);
+
+  //0.1 + 0.2 fix
+  const result = (firstNumber + secondNumber).toFixed(1);
+
+  alert(result);
+};
+
+sumButton.addEventListener('click', sum);
